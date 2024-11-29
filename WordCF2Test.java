65e89b9f86048c3e1fa040b4315cc25970a1f113
KangKang109 <minwoo5377@naver.com>
2024-11-29 11:01:23 +0900

    Add files via upload
    단어별 거리 기반 형태적 유사도

---
+++ WordCF2Test.java
@@ -0,0 +1,22 @@
+package com.travelkit.backend.service;
+/*
+build.gradle파일에 추가
+
+	implementation 'org.apache.commons:commons-text:1.10.0' // Apache Commons Text
+
+ */
+import org.apache.commons.text.similarity.LevenshteinDistance;
+
+public class WordCF2Test {
+    public static void main(String[] args) {
+        String word1 = "선글라스";
+        String word2 = "선글래스";
+
+        // Levenshtein Distance
+        LevenshteinDistance levenshtein = new LevenshteinDistance();
+        int distance = levenshtein.apply(word1, word2);
+
+        // 결과 출력
+        System.out.println("Levenshtein Distance between '" + word1 + "' and '" + word2 + "' is: " + distance);
+    }
+}
